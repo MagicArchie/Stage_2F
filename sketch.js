0bfa5f0a4dbf1439039cc722204e7da5244a34b8
Archie <149274597+MagicArchie@users.noreply.github.com>
2024-01-31 14:37:18 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -114,10 +114,9 @@ let oneUse3 = false;
 let oneUse4 = false;
 let showMessage = false;
 
-let StageSend = 2;
-localStorage.setItem('data', StageSend);
-//let Unlock = parseInt(localStorage.getItem('data'), 10);
-let Unlock = 1;
+let StageSend = "STG2";
+localStorage.setItem('Stage', StageSend);
+let Unlock = parseInt(localStorage.getItem('Complete2'), 10);
 
 function preload() {
   backgroundImage = loadImage("materials/images/Background11.jpg");
@@ -287,7 +286,7 @@ function setup() {
 function draw() {
   image(backgroundImage, 0, 0, width, height);
   
-  if (Unlock == 2) {
+  if (Unlock == "true") {
     if (oneUse4 == false) {
       keyimg.style('pointer-events', 'auto');
       notification.show();
@@ -662,9 +661,9 @@ function keyPressed() {
 
     // Check the entered code and redirect the user
     if (userCode === "KeyU") {
-      Unlock = 2;
+      Unlock = "true";
     } else if (userCode === "KeyL") {
-      Unlock = 1;
+      Unlock = "false";
     } 
   }
 }
@@ -709,7 +708,7 @@ function mousePressed() {
       mouseX <= 1126 &&
       mouseY >= 110 &&
       mouseY <= 410 &&
-      Unlock != 2
+      Unlock != "true"
     ) {
        choice.setVolume(0.1);
        choice.play();
